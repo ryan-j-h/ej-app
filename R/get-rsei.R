ac142c68a8b3edb1109577746904613fb26b179a
ryan-j-h <rjh62@duke.edu>
2020-06-11 16:26:33 -0400

    setup repo

---
+++ R/get-rsei.R
@@ -0,0 +1,14 @@
+base_url <- "ftp://newftp.epa.gov/RSEI/"
+
+# Shape Files
+
+shape_url <- "ftp://newftp.epa.gov/RSEI/Shapefiles/810m_Standard_Grid_Shapefiles/"
+
+
+# 2018 RSEI Data
+
+base_2018_url <- "ftp://newftp.epa.gov/RSEI/Version238_RY2018/SpringUpdate/Public_Release_Data"
+
+#zip_2018_url <- "ftp://newftp.epa.gov/RSEI/Version238_RY2018/SpringUpdate/Public_Release_Data/RSEIv238_spring_Public_Releases_Data.zip"
+#download.file(zip_2018_url, destfile = "data/rsei/2018/RSEI2018.zip")
+#unzip("data/rsei/2018/RSEI2018.zip", exdir = "data/rsei/2018")
